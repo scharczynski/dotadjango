1ab422950028e65152da8d40e7a1637cf869a2d4
scharczynski <scharcz@brandeis.edu>
2017-07-07 16:57:32 -0400

    support for 5 players

--- stats/forms.py
+++ stats/forms.py
@@ -1,4 +1,8 @@
 from django import forms
 
 class IdForm(forms.Form):
-    steam_id = forms.CharField(label='steam id', max_length=100)
+    steam_id1 = forms.CharField(label='steam id', max_length=100)
+    steam_id2 = forms.CharField(label='steam id', max_length=100)
+    steam_id3 = forms.CharField(label='steam id', max_length=100)
+    steam_id4 = forms.CharField(label='steam id', max_length=100)
+    steam_id5 = forms.CharField(label='steam id', max_length=100)
--- stats/views.py
+++ stats/views.py
@@ -22,27 +22,28 @@ from .forms import IdForm
 #     context = {'teams': teams, 'players':players}
 #     return render(request, 'stats/index.html', context)
 
+
 def team(request, team_id):
-    
+
     team = get_object_or_404(Team, pk=team_id)
     players = team.player_set.all()
 
-    return render(request, 'stats/team.html', {'team': team, 'players':players})
-    #return HttpResponse("You're looking at team %s." % team_id)
+    return render(request, 'stats/team.html', {'team': team, 'players': players})
+    # return HttpResponse("You're looking at team %s." % team_id)
+
 
 def player(request, player_id):
-    player = get_object_or_404(Player, pk = player_id)
-    return render(request ,'stats/player.html', {'player':player})
-
+    player = get_object_or_404(Player, pk=player_id)
+    return render(request, 'stats/player.html', {'player': player})
 
 
 # class TeamView(generic.DetailView):
 #     model = Team
 #     template_name = 'stats/team.html'
-    
+
 def match(request):
     match = requests.get('https://api.opendota.com/api/matches/3200576427')
-    return render(request, 'stats/match.html', {'match':match})
+    return render(request, 'stats/match.html', {'match': match})
 
 
 # class PlayerView(generic.DetailView):
@@ -51,7 +52,8 @@ def match(request):
 
 def index(request):
     if request.method == 'POST':
-            # create a form instance and populate it with data from the request:
+            # create a form instance and populate it with data from the
+            # request:
         form = IdForm(request.POST)
         # check whether it's valid:
         if form.is_valid():
@@ -65,21 +67,22 @@ def index(request):
         form = IdForm()
 
     return render(request, 'stats/index.html', {'form': form})
-    
+
 
 def teams(request):
     teams = Team.objects.all()
 
-    return render(request,'stats/teams.html', {'teams':teams})
+    return render(request, 'stats/teams.html', {'teams': teams})
     #template_name = 'stats/teams.html'
-    #context_object_name = 'teams'
+    # context_object_name = 'teams''
 
-def results(request):
-    print request.POST['steam_id']
-    steam_id = request.POST['steam_id']
-    player_heroes = requests.get('https://api.opendota.com/api/players/'+str(steam_id)+'/heroes?limit=1000')
+
+def parse_player(steam_id):
+    player_heroes = requests.get(
+        'https://api.opendota.com/api/players/' + str(steam_id) + '/heroes?date=180&lobby_type=7')
     hero_info = requests.get('https://api.opendota.com/api/heroStats')
-    player = requests.get('https://api.opendota.com/api/players/'+str(steam_id))
+    player = requests.get(
+        'https://api.opendota.com/api/players/' + str(steam_id))
     parsed_player = player.json()
     parsed_player_heroes = player_heroes.json()
     parsed_hero_info = hero_info.json()
@@ -90,10 +93,8 @@ def results(request):
     top_compares = []
     comparedict = {}
 
-   
-
     for j in parsed_hero_info:
-        namelist[j["id"]]  =  j["localized_name"]
+        namelist[j["id"]] = j["localized_name"]
         fivewin = float(j['5000_win']) / j['5000_pick']
         fivek_dict[j["id"]] = fivewin
 
@@ -102,40 +103,57 @@ def results(request):
             player_winrate = float(i['win']) / i['games']
         else:
             player_winrate = 0
-        #print i['win'] / i['games']
-        #rint i['games']
+        # print i['win'] / i['games']
+        # rint i['games']
         hero_id = int(i["hero_id"])
         compare_winrate = player_winrate / fivek_dict[hero_id]
-        hero_wins.append((player_winrate, namelist[int(hero_id)], compare_winrate))
+        hero_wins.append(
+            (player_winrate, namelist[int(hero_id)], compare_winrate))
         comparedict[hero_id] = compare_winrate
-        top_compares.append((compare_winrate, namelist[int(hero_id)], i['games']))
-        
+        top_compares.append(
+            (compare_winrate, namelist[int(hero_id)], i['games']))
 
     solo_mmr = parsed_player['solo_competitive_rank']
     profile = parsed_player['profile']
     name = profile['name']
     persona_name = profile['personaname']
 
-
     top_compares.sort()
-    
-    #print top_compares[0][3]
-    filtered_compares = list(filter(lambda x: x[2] > 10, top_compares))
-    filtered_compares.sort(key=lambda x: x[0], reverse=True)
-    print filtered_compares
-    print len(top_compares)
-    print len(filtered_compares)
-    #print comparedict
 
-    return render(request, 'stats/results.html', {'player':player, 'solo_mmr': solo_mmr, 'name':name, 'persona_name':persona_name, 'top3':hero_wins[:3], 'top3_relative': filtered_compares[:5]})
+    # print top_compares[0][3]
+    filtered_compares = list(filter(lambda x: x[2] > 5, top_compares))
+    filtered_compares.sort(key=lambda x: x[0], reverse=True)
+
+    return persona_name, solo_mmr, filtered_compares
+
+
+def results(request):
+    #print request.POST['steam_id']
+    #steam_id = request.POST['steam_id']
+    steam_id1, steam_id2, steam_id3, steam_id4, steam_id5 = request.POST['steam_id1'], request.POST[
+        'steam_id2'], request.POST['steam_id3'], request.POST['steam_id4'], request.POST['steam_id5']
+
+    data1 = parse_player(steam_id1)
+    data2 = parse_player(steam_id2)
+    data3 = parse_player(steam_id3)
+    data4 = parse_player(steam_id4)
+    data5 = parse_player(steam_id5)
+
+    # return render(request, 'stats/results.html', {'player':player,
+    # 'solo_mmr': solo_mmr, 'name':name, 'persona_name':persona_name,
+    # 'top3':hero_wins[:3], 'top3_relative': filtered_compares[:5]})
+    return render(request, 'stats/results.html', {'data1': data1, 'data2': data2, 'data3': data3, 'data4': data4, 'data5': data5})
+
 
 def lookup(request, steam_id):
-    player = requests.get('https://api.opendota.com/api/players/'+str(steam_id))
-    heroes = requests.get('https://api.opendota.com/api/players/'+str(steam_id)+'/heroes')
+    player = requests.get(
+        'https://api.opendota.com/api/players/' + str(steam_id))
+    heroes = requests.get(
+        'https://api.opendota.com/api/players/' + str(steam_id) + '/heroes')
     hero_stats = requests.get('https://api.opendota.com/api/heroStats')
     parsed_heroes = heroes.json()
     parsed_player = player.json()
-    parsed_hero_stats= hero_stats.json()
+    parsed_hero_stats = hero_stats.json()
     hero_wins = []
     namelist = {}
     fivek_winrate = []
@@ -143,23 +161,21 @@ def lookup(request, steam_id):
     top_compares = []
     comparedict = {}
 
-   
-
     for j in parsed_hero_stats:
-        namelist[j["id"]]  =  j["localized_name"]
+        namelist[j["id"]] = j["localized_name"]
         fivewin = float(j['5000_win']) / j['5000_pick']
         fivek_dict[j["id"]] = fivewin
 
     for i in parsed_heroes:
         player_winrate = float(i['win']) / i['games']
-        #print i['win'] / i['games']
-        #rint i['games']
+        # print i['win'] / i['games']
+        # rint i['games']
         hero_id = int(i["hero_id"])
         compare_winrate = player_winrate / fivek_dict[hero_id]
-        hero_wins.append((player_winrate, namelist[int(hero_id)], compare_winrate))
+        hero_wins.append(
+            (player_winrate, namelist[int(hero_id)], compare_winrate))
         comparedict[hero_id] = compare_winrate
         top_compares.append(compare_winrate)
-        
 
     solo_mmr = parsed_player['solo_competitive_rank']
     profile = parsed_player['profile']
@@ -168,7 +184,4 @@ def lookup(request, steam_id):
 
     top_compares.sort()
 
-
-
-
-    return render(request, 'stats/lookup.html', {'player':player, 'solo_mmr': solo_mmr, 'name':name, 'persona_name':persona_name, 'top3':hero_wins[:3], 'top3_relative': top_compares[:-3]})
+    return render(request, 'stats/lookup.html', {'player': player, 'solo_mmr': solo_mmr, 'name': name, 'persona_name': persona_name, 'top3': hero_wins[:3], 'top3_relative': top_compares[:-3]})
